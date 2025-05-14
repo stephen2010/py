c2e009fcfa71074be0deeabac179cbb62c26c4c8
stephen2010 <mancini1984@163.com>
2025-05-14 09:18:04 +0800

    fresh/compat

--- routes/py.tsx
+++ routes/py.tsx
@@ -1,4 +1,20 @@
-import { defineRoute, Handlers } from "fresh/server.ts";
+import { defineRoute, Handlers } from "fresh/compat";
+
+
+var title = "";
+var url = "";
+
+export const handler: Handlers = {
+  async POST(req, _ctx) {
+    const form = await req.formData();
+    title = form.get("title");
+    url = form.get("url");
+
+
+    return Response.json({title, url});
+
+  },
+};
 
 
 const SUBMIT_STYLES =
@@ -16,7 +32,46 @@ export default defineRoute(async (_req, _ctx) => {
           </p>
         </div>
         <div class="flex flex-col md:flex-row gap-8 md:gap-16 md:items-center">
+          <form class="flex-1 flex flex-col justify-center" method="post">
+            <div>
+              <label
+                htmlFor="submit_title"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                {title}
+              </label>
 
+              <input
+                id="submit_title"
+                class="input-styles w-full mt-2"
+                type="text"
+                name="title"
+                required
+                placeholder="Title"
+              />
+            </div>
+
+            <div class="mt-4">
+              <label
+                htmlFor="submit_url"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                {url}
+              </label>
+              <input
+                id="submit_url"
+                class="input-styles w-full mt-2"
+                type="text"
+                name="url"
+                required
+                placeholder="https://xxx.xxx.com"
+              />
+            </div>
+
+            <div class="w-full rounded-lg bg-gradient-to-tr from-secondary to-primary p-px mt-8">
+              {<button type="submit" class={SUBMIT_STYLES}>Submit</button>}
+            </div>
+          </form>
         </div>
       </main>
     </>
